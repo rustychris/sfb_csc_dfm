042a6795e3b3d805cb6b55be1f8fd50ea3ff6c54
Rusty Holleman <rustychris@gmail.com>
2023-03-10 10:04:56 -0600

    git updates for generated files

--- model/run_sfb_csc_test.py
+++ model/run_sfb_csc_test.py
@@ -10,7 +10,7 @@ import shutil
 
 model=sfb_csc.SfbCsc(run_start=np.datetime64("2019-04-01"),
                      run_stop =np.datetime64("2019-04-10"),
-                     run_dir="testrun000",
+                     run_dir="data_testrun000",
                      extra_scripts=[__file__])
 
 model.write()
